a3ebd1822e0e28d3d3a23ede2eeb41b3cdf9a022
bkisembo <bkisembo8@icloud.com>
2025-09-06 14:17:45 +0100

    Add files via upload
    initial commit

---
+++ 01-potters.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS potters;
+
+CREATE TABLE potters (
+    potters_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    password TEXT NOT NULL,
+    studio_postcode TEXT NOT NULL
+);
+
+INSERT INTO potters (username, firstname, lastname, password, studio_postcode) VALUES
+('KJones', 'Keith', 'Jones', 'test', 'CT5'),
+('RichM', 'Rich', 'Miller', 'test', 'CR3'),
+('SamP', 'Samuel', 'Pipi', 'IloveAWS', 'EN1');
+
